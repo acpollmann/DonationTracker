67bd3e8b8fa7fa00f014f1b2d3eaa3b9233959ce
Amy Pollmann <amy.pollmann@gmail.com>
2018-09-21 16:48:42 -0400

    Added registration buttons and nav

--- app/src/main/java/edu/gatech/cs2340/donationtracker/RegisterActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/RegisterActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cs2340.donationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        configureBackButton();
+        configureSaveRegistrationButton();
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void configureSaveRegistrationButton() {
+        Button backButton = (Button) findViewById(R.id.saveRegistrationButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
     }
 }
