2c18b36e936fb2bfce9d12ad5275b75caa2ff432
unknown <dadabs1134@gmail.com>
2018-09-30 21:45:04 -0500

    UserMap, LoginActivity, and RegisterActivity are updated

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Model/UserMap.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Model/UserMap.java
@@ -3,6 +3,8 @@ package edu.gatech.cs2340.donationtracker.Model;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.gatech.cs2340.donationtracker.Controller.RegisterActivity;
+
 /*
 * Class UserMap for storing data of various types of users
 * Created by Aviva Smith 9/30/18
@@ -10,16 +12,15 @@ import java.util.Map;
 
 public class UserMap {
 
-    public static void main(String[] args) {
-        String username = null;
-        String password = null;
-        String email = null;
-        User user = new User(username, password, email);
-
-        /*Hashmap stores users by email and password key-value pairs so
-        * system can check if user is in database.*/
+    public static Map<String, String> usersByEmail(String username, String email, String password) {
         HashMap<String, String> UsersByEmail = new HashMap<>();
-        UsersByEmail.put(user.getEmail(), user.getPassword());
+        UsersByEmail.put(email, password);
+        return UsersByEmail;
     }
 
+    public static Map<String, String> usersByName(String username, String email, String password) {
+        HashMap<String, String> UsersByName = new HashMap<>();
+        UsersByName.put(username, password);
+        return UsersByName;
+    }
 }
