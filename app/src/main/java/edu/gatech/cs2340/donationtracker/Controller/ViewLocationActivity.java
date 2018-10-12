cef9dd3ae5e25839552baa47d6428e6d23c8213c
Aljon Pineda <aljonjbpineda@gmail.com>
2018-10-12 16:01:25 -0400

    Reading CSV file is now done on startup

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/ViewLocationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/ViewLocationActivity.java
@@ -50,7 +50,7 @@ public class ViewLocationActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.locationitem_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
-        readSDFile();
+//        readSDFile();
         // add data for displaying in expandable list view
         loadData();
 
@@ -202,36 +202,36 @@ public class ViewLocationActivity extends AppCompatActivity {
         return groupPosition;
     }
 
-    private void readSDFile() {
-        //ListModel model = ListModel.INSTANCE;
-
-        try {
-            //Open a stream on the raw file
-            InputStream is = getResources().openRawResource(R.raw.locationdata);
-            //From here we probably should call a model method and pass the InputStream
-            //Wrap it in a BufferedReader so that we get the readLine() method
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-
-            String line;
-            br.readLine(); //get rid of header line
-            while ((line = br.readLine()) != null) {
-                Log.d(MainActivity.TAG, line);
-                String[] tokens = line.split(",");
-                int key = Integer.parseInt(tokens[0]);
-                double latitude = Double.parseDouble(tokens[2]);
-                double longitude = Double.parseDouble(tokens[3]);
-                int zip = Integer.parseInt(tokens[7]);
-                if (model.findItemById(key) == null) {
-                    model.addItem(new LocationItem(key, tokens[1], latitude, longitude,
-                            tokens[4], tokens[5], tokens[6], zip,
-                            tokens[8], tokens[9], tokens[10]));
-                }
-            }
-            br.close();
-        } catch (IOException e) {
-            Log.e(MainActivity.TAG, "error reading assets", e);
-        }
-    }
+//    private void readSDFile() {
+//        //ListModel model = ListModel.INSTANCE;
+//
+//        try {
+//            //Open a stream on the raw file
+//            InputStream is = getResources().openRawResource(R.raw.locationdata);
+//            //From here we probably should call a model method and pass the InputStream
+//            //Wrap it in a BufferedReader so that we get the readLine() method
+//            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+//
+//            String line;
+//            br.readLine(); //get rid of header line
+//            while ((line = br.readLine()) != null) {
+//                Log.d(MainActivity.TAG, line);
+//                String[] tokens = line.split(",");
+//                int key = Integer.parseInt(tokens[0]);
+//                double latitude = Double.parseDouble(tokens[2]);
+//                double longitude = Double.parseDouble(tokens[3]);
+//                int zip = Integer.parseInt(tokens[7]);
+//                if (model.findItemById(key) == null) {
+//                    model.addItem(new LocationItem(key, tokens[1], latitude, longitude,
+//                            tokens[4], tokens[5], tokens[6], zip,
+//                            tokens[8], tokens[9], tokens[10]));
+//                }
+//            }
+//            br.close();
+//        } catch (IOException e) {
+//            Log.e(MainActivity.TAG, "error reading assets", e);
+//        }
+//    }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
 
@@ -259,7 +259,7 @@ public class ViewLocationActivity extends AppCompatActivity {
             holder.mItem = mValues.get(position);
             holder.mIdView.setText("" + mValues.get(position).getKey());
             holder.mContentView.setText(mValues.get(position)._getLocationName());
-            readSDFile();
+//            readSDFile();
 
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/WelcomeScreenActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/WelcomeScreenActivity.java
@@ -3,9 +3,18 @@ package edu.gatech.cs2340.donationtracker.Controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import edu.gatech.cs2340.donationtracker.Model.ListModel;
+import edu.gatech.cs2340.donationtracker.Model.LocationItem;
 import edu.gatech.cs2340.donationtracker.R;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
@@ -17,6 +26,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         configureRegistrationButton();
         configureSkipButton();
     }
+
     public void login(View view) {
         Intent changeLogin = new Intent(this, LoginActivity.class);
         startActivity(changeLogin);
