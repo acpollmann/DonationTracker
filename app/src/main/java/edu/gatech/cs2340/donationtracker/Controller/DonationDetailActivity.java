0a54f5ba12a5f99bd9e8f1693469d9e4a792bad4
Aljon Pineda <aljonjbpineda@gmail.com>
2018-10-15 12:12:05 -0400

    donation detail

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/DonationDetailActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/DonationDetailActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cs2340.donationtracker.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import edu.gatech.cs2340.donationtracker.R;
 
 public class DonationDetailActivity extends AppCompatActivity {
@@ -10,5 +13,10 @@ public class DonationDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_detail);
+    }
+
+    public void onBackButtonPressed(View view) {
+        Intent intent = new Intent(this, ViewDonationsActivity.class);
+        startActivity(intent);
     }
 }
