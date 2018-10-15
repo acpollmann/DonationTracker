4aad0732b43c8d98b7b4f0f567992169c88c4e4c
unknown <dadabs1134@gmail.com>
2018-10-15 14:00:40 -0500

    All info shows up

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/LocationDetailActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/LocationDetailActivity.java
@@ -11,6 +11,10 @@ import edu.gatech.cs2340.donationtracker.R;
 
 public class LocationDetailActivity extends AppCompatActivity {
     private TextView mName;
+    private TextView Address;
+    private TextView latitudeLongitude;
+    private TextView phoneNumber;
+    private TextView webSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,24 @@ public class LocationDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location_detail);
 
         mName = findViewById(R.id.name);
+        Address = findViewById(R.id.address);
+        latitudeLongitude = findViewById(R.id.latitute_longitude);
+        phoneNumber = findViewById(R.id.phone_number);
+        webSite = findViewById(R.id.website);
 
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
         String name = b.getString("name");
+        String address = b.getString("address");
+        String lat_long = b.getString("latitude_longitude");
+        String phone_number = b.getString("phone number");
+        String website = b.getString("website");
 
         mName.setText(name);
+        Address.setText(address);
+        latitudeLongitude.setText(lat_long);
+        phoneNumber.setText(phone_number);
+        webSite.setText(website);
     }
 
     public void onBackButtonPressed(View view) {
