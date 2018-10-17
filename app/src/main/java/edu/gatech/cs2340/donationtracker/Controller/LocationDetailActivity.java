ef2d4c517a313995d788a3bfd180d33015513008
unknown <dadabs1134@gmail.com>
2018-10-16 22:11:06 -0500

    Location Details Completely working

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/LocationDetailActivity.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/LocationDetailActivity.java
@@ -11,10 +11,11 @@ import edu.gatech.cs2340.donationtracker.R;
 
 public class LocationDetailActivity extends AppCompatActivity {
     private TextView mName;
-    private TextView Address;
+    private TextView mType;
+    private TextView mAddress;
     private TextView latitudeLongitude;
-    private TextView phoneNumber;
-    private TextView webSite;
+    private TextView mPhoneNumber;
+    private TextView mWebSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +23,27 @@ public class LocationDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location_detail);
 
         mName = findViewById(R.id.name);
-        Address = findViewById(R.id.address);
+        mType = findViewById(R.id.type);
+        mAddress = findViewById(R.id.address);
         latitudeLongitude = findViewById(R.id.latitute_longitude);
-        phoneNumber = findViewById(R.id.phone_number);
-        webSite = findViewById(R.id.website);
+        mPhoneNumber = findViewById(R.id.phone_number);
+        mWebSite = findViewById(R.id.website);
 
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
         String name = b.getString("name");
+        String type = b.getString("type");
         String address = b.getString("address");
-        String lat_long = b.getString("latitude_longitude");
-        String phone_number = b.getString("phone number");
+        String lat_long = b.getString("latitudeLongitude");
+        String phone_number = b.getString("phoneNumber");
         String website = b.getString("website");
 
         mName.setText(name);
-        Address.setText(address);
+        mType.setText(type);
+        mAddress.setText(address);
         latitudeLongitude.setText(lat_long);
-        phoneNumber.setText(phone_number);
-        webSite.setText(website);
+        mPhoneNumber.setText(phone_number);
+        mWebSite.setText(website);
     }
 
     public void onBackButtonPressed(View view) {
