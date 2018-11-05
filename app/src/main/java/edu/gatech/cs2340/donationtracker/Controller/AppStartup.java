6bedf98280ca531e256692e23cc8dda17283eb61
leighannrak <leighannraksasouk@gmail.com>
2018-11-05 11:29:59 -0500

    AddStartup javadocs

--- app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/AppStartup.java
+++ app/src/main/java/edu/gatech/cs2340/donationtracker/Controller/AppStartup.java
@@ -19,10 +19,22 @@ import edu.gatech.cs2340.donationtracker.Model.User;
 import edu.gatech.cs2340.donationtracker.Model.UserSet;
 import edu.gatech.cs2340.donationtracker.R;
 
+/**
+ * Implementation that will initiate ListModel and FireStore.
+ *
+ * @author Group 71B
+ * @version 1.0
+ */
+
 public class AppStartup extends Application {
     private ListModel model;
     private FirestoreManager firestoreManager;
 
+    /**
+     * On the creation of the app it will create the list model
+     * and set up the fireStore. It will add the lists of donations
+     * and locations to fireStore.
+     */
     @Override
     public void onCreate() {
         super.onCreate();
